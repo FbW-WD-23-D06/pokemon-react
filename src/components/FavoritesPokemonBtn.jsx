5a741cf09d5a714b725ab35759a87b449f18146f
giacomotolari <tolarigiacomo@gmail.com>
2024-01-08 15:27:14 +0100

    feature: add siable add favorite logic

--- src/components/FavoritesPokemonBtn.jsx
+++ src/components/FavoritesPokemonBtn.jsx
@@ -1,19 +1,33 @@
 /* eslint-disable react/prop-types */
 export default function FavoritesPokemonBtn({ pokemonData }) {
   console.log("pokemonData:", pokemonData);
-  let favoritePokemons = JSON.parse(localStorage.getItem("favoritePokemons")) || [];
+  let favoritePokemons =
+    JSON.parse(localStorage.getItem("favoritePokemons")) || [];
   // 1 wenn wir klicken den button, werden wir ein pokemon in dem localStorage array HINZUFÜGEN
 
   // UND EIN EVENT UM DAS ZU SPEICHERN
 
-  const addFavorite = () => {
-    localStorage.setItem("favoritePokemons", JSON.stringify([...favoritePokemons, pokemonData]));
-  };
+  const currentPokemonAlreadySaved = favoritePokemons.includes(
+    pokemonData.name
+  );
 
+  const addFavorite = () => {
+    if (currentPokemonAlreadySaved) {
+      alert("already saved");
+      return;
+    }
+    console.log("addFavorite");
+    localStorage.setItem(
+      "favoritePokemons",
+      JSON.stringify([...favoritePokemons, pokemonData.name])
+    );
+  };
 
   return (
     <>
-      <button onClick={addFavorite}>Add Favorite</button>
+      <button onClick={addFavorite} /*disabled={currentPokemonAlreadySaved}*/>
+        Add Favorite
+      </button>
     </>
   );
 }
